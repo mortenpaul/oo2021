d23aa5ccc59d8e261501d4da6838b78a61fea771
Morten-Paul Mühlberg <70900314+mortenpaul@users.noreply.github.com>
2021-02-02 10:51:27 +0200

    Tund1

---
+++ w01_funktsioon/Funktsioonid2.java
@@ -0,0 +1,9 @@
+class Funktsioonid2 {
+	public static double taksosoit(double i){
+		return 3+i*0.80;
+	}
+    public static void main(String[] args) {
+		System.out.println(taksosoit(10));
+
+		}
+    }
